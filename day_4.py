f7dcffee224b552ddf6b06ecc0a45cb87a79fbbf
Jessseee <jssvssr2000@gmail.com>
2020-12-09 21:53:48 +0100

    Refactored Day 4

--- day_4.py
+++ day_4.py
@@ -1,56 +1,80 @@
 # Day 4 Advent of Code
 # Passport validation
 import re
-from helpers import color_text
-
-file_name = "input/input_day_4.txt"
+from helpers import *
 
 
-def passport_scanner(required_keys):
+def scan_passports(input, validate):
     """
-    :param required_keys: A tuple of required passport fields and regex to validate the field values
+    :param input: List of all unvalidated passports
+    :param validate: Method to validate passports
     :return: Lists of valid and invalid passports
     """
     valid_passports = []
     invalid_passports = []
-    with open(file_name) as f:
+
+    for passport in input:
+        key_value_pairs = passport.replace('\n', ' ').split(' ')
         passport = {}
-        for line in f.readlines():
-            if line != "\n":
-                key_value_pairs = line.split(' ')
-                for pair in key_value_pairs:
-                    key, value = pair.split(':')
-                    # Remove the possible newline from end of field value
-                    passport[key] = value.replace('\n', '')
-            else:
-                if validate_passport(passport, required_keys):
-                    valid_passports.append(passport)
-                else:
-                    invalid_passports.append(passport)
-                passport = {}
-    f.close()
+        for pair in key_value_pairs:
+            key, value = pair.split(':')
+            passport[key] = value
+        if validate(passport):
+            valid_passports.append(passport)
+        else:
+            invalid_passports.append(passport)
+    print_passports(valid_passports, invalid_passports)
     return valid_passports, invalid_passports
 
 
-def validate_passport(passport, required_keys):
+def validate_fields(passport):
     """
     :param passport: The passport to validate
-    :param required_keys: A tuple of the required fields and regex to validate the field values
     :return: If all passport fields are present and valid
     """
-    if all(key in passport for key in required_keys):
-        for key, value in required_keys.items():
+    if all(key in passport for key in REQUIRED_KEYS):
+        for key, value in REQUIRED_KEYS.items():
             if value != '':
                 regex = re.compile(value)
                 if not regex.match(passport[key]):
                     return False
         return True
-    else:
-        return False
+    return False
+
+
+def check_required_fields(passport):
+    """
+    :param passport: The passport to validate
+    :return: If all passport fields are present
+    """
+    return all(key in passport for key in REQUIRED_KEYS)
+
+
+def print_passports(valid_passports, invalid_passports, print_all=False):
+    """
+    :param valid_passports: List of valid passports
+    :param invalid_passports: List of invalid passports
+    :param print_all: Should print all passport data
+    :return:
+    """
+    print(f'valid passports: {color_text(len(valid_passports), 32)}')
+    print(f'invalid passports: {color_text(len(invalid_passports), 31)}')
+    if print_all:
+        print(color_text('\nVALID PASSPORTS', 32))
+        for valid_passport in valid_passports:
+            for key in REQUIRED_KEYS:
+                print(color_text(f'{key}: {valid_passport[key]} |', 37), end=' ')
+            print('\n')
+        print(color_text('\nINVALID PASSPORTS', 31))
+        for invalid_passport in invalid_passports:
+            for key in REQUIRED_KEYS:
+                if key in invalid_passport:
+                    print(color_text(f'{key}: {invalid_passport[key]} |', 37), end=' ')
+            print('\n')
 
 
 if __name__ == '__main__':
-    required_keys = {
+    REQUIRED_KEYS = {
         'pid': r'\b\d{9}\b',
         'byr': r'19[2-9]\d|200[0-2]',
         'iyr': r'20(1\d|20)',
@@ -59,18 +83,8 @@ if __name__ == '__main__':
         'hcl': r'#[0-9a-f]{6}',
         'ecl': r'amb|blu|brn|gry|grn|hzl|oth'
     }
-
-    valid_passports, invalid_passports = passport_scanner(required_keys)
-
-    print(color_text(f'{len(invalid_passports)} invalid passports:', 31))
-    for invalid_passport in invalid_passports:
-        for key in required_keys:
-            if key in invalid_passport:
-                print(f'{key}: {invalid_passport[key]} |', end=' ')
-        print('\n')
-
-    print(color_text(f'\n{len(valid_passports)} valid passports:', 32))
-    for valid_passport in valid_passports:
-        for key in required_keys:
-            print(f'{key}: {valid_passport[key]} |', end=' ')
-        print('\n')
+    passports = import_input().read().split('\n\n')
+    print("Check required fields:")
+    scan_passports(passports, check_required_fields)
+    print("\nCheck required fields and validity:")
+    scan_passports(passports, validate_fields)
