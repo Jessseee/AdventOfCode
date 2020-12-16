aa86bb52748c4a92f1d007796765ce4548c5e3f0
Jessseee <jssvssr2000@gmail.com>
2020-12-16 16:39:21 +0100

    Day 15: Rambunctious Recitation

---
+++ day_15.py
@@ -0,0 +1,25 @@
+# Day 15 Advent of Code
+# Memory games
+from helpers import *
+
+
+def play_memory_game(start_numbers, number_of_turns):
+    previous = {number: i + 1 for i, number in enumerate(start_numbers)}
+    turn = len(previous)
+    number = int(start_numbers[-1])
+    while turn < number_of_turns:
+        if not previous.get(number, None):
+            previous[number] = turn
+            number = 0
+        else:
+            last_seen = previous[number]
+            previous[number] = turn
+            number = turn - last_seen
+        turn += 1
+        print(f'{turn} \t {number}')
+
+
+if __name__ == '__main__':
+    start_numbers = [int(i) for i in import_input().read().split(',')]
+    play_memory_game(start_numbers, 2020)
+    play_memory_game(start_numbers, 30000000)
