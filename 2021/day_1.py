1e5d32a0caa6fe8ce000c2e69621bc0d65b4bcf4
Jessseee <jssvssr2000@gmail.com>
2021-12-01 15:17:09 +0900

    Day 1: Sonar Sweep

---
+++ 2021/day_1.py
@@ -0,0 +1,9 @@
+from helpers import *
+
+
+if __name__ == '__main__':
+    inputs = [int(line.rstrip('\n')) for line in import_input().readlines()]
+    single_increases = sum(inputs[i] < inputs[i+1] for i in range(len(inputs)-1))
+    print(f'There are {color_text(single_increases, 32)} increases in the single depth measurements.')
+    sweep_increases = sum(sum(inputs[i:i+3]) < sum(inputs[i+1:i+4]) for i in range(len(inputs)-1))
+    print(f'There are {color_text(sweep_increases, 32)} increases in the sliding window depth measurements.')
