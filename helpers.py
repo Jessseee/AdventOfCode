b422a6dc3437243d424fd2142e3d2ae129994b70
Jessseee <jssvssr2000@gmail.com>
2021-12-05 12:23:24 +0900

    Added documentation to helper functions

--- helpers.py
+++ helpers.py
@@ -14,6 +14,11 @@ def color_text(text, color_code):
 
 
 def import_input(example=False):
+    """
+    :param bool example: Should import example input instead
+    :return: Stream of input file
+    Import input for day of puzzle
+    """
     day = os.path.basename(sys.argv[0]).split('.')[0]
     if example:
         return open("input/example_input_" + day + ".txt")
@@ -22,14 +27,33 @@ def import_input(example=False):
 
 
 def replace_chr(i, char, string):
+    """
+    :param int i: Index of character to replace
+    :param str char: Character(s) to place in string
+    :param str string: String to replace character in
+    :return: String with character replaced
+    Replace a character in a string
+    """
     return string[:i] + char + string[i + 1:]
 
 
 def replace_chrs(span, char, string):
+    """
+    :param list span: Start and end of span to replace
+    :param str char: Character(s) to place in string
+    :param str string: String to replace characters in
+    :return: String with characters replaced
+    Replace a span of characters in a string
+    """
     return string[:span[0]] + char + string[span[1]:]
 
 
 def print_2d_array(array, el_length):
+    """
+    :param list[list] array: 2d array to print
+    :param int el_length: the length of each element in the array
+    Nicely print a 2d array
+    """
     for row in array:
         for el in row:
             if not el:
