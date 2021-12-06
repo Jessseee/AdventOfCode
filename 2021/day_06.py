3fd9e3c3e589eb5025f477d0d353db35641a3eab
Jessseee <jssvssr2000@gmail.com>
2021-12-06 20:29:02 +0900

    Added prettier output to day 6 solution

--- 2021/day_06.py
+++ 2021/day_06.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
     for i in init_fishes:
         fishes[i] += 1
 
-    print('day', 0, sum(fishes))
     for day in range(256):
         spawn = fishes.pop(0)
         fishes[6] += spawn
         fishes.append(spawn)
-        print('day', day+1, sum(fishes))
+        if day == 79:
+            print(f"After {color_text('80', 32)} days there are {color_text('{:,}'.format(sum(fishes)), 31)} angler fish!")
+    print(f"After {color_text('256', 32)} days there are {color_text('{:,}'.format(sum(fishes)), 31)} angler fish!")
