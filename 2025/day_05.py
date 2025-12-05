14ccc511f8a091055b353db6bb73cee2a098c56e
Jesse Visser <dev@jesse-visser.nl>
2025-12-05 14:07:45 +0100

    Day 05: Cafeteria

---
+++ 2025/day_05.py
@@ -0,0 +1,55 @@
+# Day 05 of Advent of Code 2025
+# Cafeteria
+
+import re
+import unittest
+
+from aoc.helpers import import_input, parse_input
+
+
+def parser(inputs):
+    ranges, ids = map(str.split, inputs.split("\n\n"))
+    ranges = sorted([tuple(map(int, re.findall(r"\d+", line))) for line in ranges])
+    ranges = [(start, end + 1) for start, end in ranges]  # Make half-open interval
+    ids = list(map(int, ids))
+    return ranges, ids
+
+
+@parse_input(parser)
+def part1(inputs):
+    total = 0
+    ranges, ids = inputs
+    for id in ids:
+        if any(start <= id < end for start, end in ranges):
+            total += 1
+    return total
+
+
+@parse_input(parser)
+def part2(inputs):
+    total = 0
+    ranges, _ = inputs
+    end_points = sorted(set(sum(ranges, ())))  # Flattened sorted list of unique range end-points
+    for i in range(len(end_points)-1):
+        a, b = end_points[i], end_points[i+1]
+        if any(a >= start and b <= end for start, end in ranges):
+            total += b - a
+    return total
+
+
+class Tests202505(unittest.TestCase):
+    def test_part1(self):
+        inputs = "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32"
+        expected = 3
+        self.assertEqual(expected, part1(inputs))
+
+    def test_part2(self):
+        inputs = "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32"
+        expected = 14
+        self.assertEqual(expected, part2(inputs))
+
+
+if __name__ == "__main__":
+    inputs = import_input()
+    print("part 1:", part1(inputs))
+    print("part 2:", part2(inputs))
